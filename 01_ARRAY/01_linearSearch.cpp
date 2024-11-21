d9f08635ef522c83cf0e6de2cca8b6ace55407c9
Anshu-keshri <anshukeshri543@gmail.com>
2024-11-21 22:40:55 +0000

    code for linear search

--- 01_ARRAY/01_linearSearch.cpp
+++ 01_ARRAY/01_linearSearch.cpp
@@ -1,14 +1,33 @@
 #include<iostream>
 using namespace std;
 
+bool linearSearch(int arr[],int size,int target){
+    for(int i=0;i<size;i++){
+        if(arr[i]==target){
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
     int arr[5]={2,4,6,8,10};
     int target=8;
-    for(int i=0;i<5;i++){
-        if(arr[i]==target){
-            cout<<"target found succesfully..... ";
-            break;
-        }
+    // for(int i=0;i<5;i++){
+    //     if(arr[i]==target){
+    //         cout<<"target found succesfully..... ";
+    //         break;
+    //     }
+    // }
+
+    bool ans=linearSearch(arr,5,target);
+    if(ans==1){
+        cout<<"element found....";
     }
+    else{
+        cout<<"element not found.....";
+    }
+
+
 }
