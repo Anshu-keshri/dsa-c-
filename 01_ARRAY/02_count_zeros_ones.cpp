f6b108679098de88638129c820a48a25bd6f001b
Anshu-keshri <anshukeshri543@gmail.com>
2024-11-21 22:54:28 +0000

    code to count no of zeros and ones ina an array

---
+++ 01_ARRAY/02_count_zeros_ones.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void count(int arr[],int size){
+
+    int zeroCount=0;
+    int oneCount=0;
+
+    for(int i=0;i<size;i++){
+        if(arr[i]==0){
+            zeroCount=zeroCount+1;
+        }
+
+        else{
+            oneCount=oneCount+1;
+        }
+    }
+    cout<<"no of zeros in this array is: "<<zeroCount<<"\n";
+    cout<<"no of ones in this array is: "<<oneCount<<"\n";
+
+}
+
+int main()
+{
+    int arr[7]={0,0,1,1,0,1,0};
+    count(arr,7);
+
+}
