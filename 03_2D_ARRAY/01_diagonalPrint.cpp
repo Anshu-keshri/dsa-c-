3178e8c1b5daae189681cf37d9de56ec2238f9ee
Anshu-keshri <anshukeshri543@gmail.com>
2024-11-22 20:48:19 +0000

    code to print the opposite diagonal elements of 2D array

--- 03_2D_ARRAY/01_diagonalPrint.cpp
+++ 03_2D_ARRAY/01_diagonalPrint.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main()
 {
-    int arr[3][3]={
-                    {10,20,30},
-                    {40,50,60},
-                    {70,80,90}
+    int arr[5][5]={
+                    {10,20,30,40,50},
+                    {40,50,60,70,80},
+                    {70,80,90,10,20},
+                    {20,40,60,10,80},
+                    {30,40,50,60,70}
                   };
-    int rowSize=3;
-    int colSize=3;
+    int rowSize=5;
+    int colSize=5;
     
     for(int i=0,j=colSize-1 ;i<rowSize,j>=0 ;i++,j--){
         cout<<arr[i][j]<<" ";
