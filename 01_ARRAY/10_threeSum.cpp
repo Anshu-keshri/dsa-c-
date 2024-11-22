6ae65976085c9d96b9c80394ec64f59b9cc06397
Anshu-keshri <anshukeshri543@gmail.com>
2024-11-22 03:08:32 +0000

    three sum problem

---
+++ 01_ARRAY/10_threeSum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+    void threeSum(int arr[],int size,int target){
+        
+    }
+
+int main()
+{
+    int arr[]={1,4,7,3,9,2};
+    int target=11;
+    int size=sizeof(arr)/sizeof(arr[0]);
+}
