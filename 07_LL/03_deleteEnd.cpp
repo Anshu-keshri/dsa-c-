a93f1ed957cbc2d6c605b2f6d0ca3c113f4cf49a
Anshu-keshri <anshukeshri543@gmail.com>
2025-02-08 08:32:08 +0000

    delete end node of the LL.

---
+++ 07_LL/03_deleteEnd.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    public:
+    node(int data,node* next){
+        this->data=data;
+        this->next=next;
+    }
+
+    public:
+    node(int data){
+        this->data=data;
+        this->next=nullptr;
+    }
+};
+
+node* arrToLL(vector<int>&arr){
+    node* head=new node(arr[0]);
+    node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        node* temp=new node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
+void deleteEnd(node* &head){
+    node* current=head;
+    while(current->next->next!=nullptr){
+        current=current->next;
+    }
+    node* end=current->next;
+    current->next=nullptr;
+    free(end);
+}
+
+void printList(node* head){
+    node* temp=head;
+    while(temp!=nullptr){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<"\n";
+}
+
+int main()
+{
+    vector<int>arr={2,3,4,5,6};
+    node* head=arrToLL(arr);
+    cout<<"head of this ll is: "<<head->data<<"\n";
+
+    cout<<"ll before deletion: ";
+    printList(head);
+
+    deleteEnd(head);
+
+    cout<<"ll after deletion: ";
+    printList(head);
+
+
+}
